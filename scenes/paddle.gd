3ff68dfb9277dacf4229735a021b0a6f73de3692
Jaber Alsalamah <SWE.Jaber@gmail.com>
2024-11-22 19:29:50 +0300

    implement paddle movement

---
+++ scenes/paddle.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var speed: float = 60.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	move(delta)
+
+
+func move(delta: float) -> void:
+	if Input.is_action_pressed("Left"):
+		position.x += -speed * delta
+
+	elif Input.is_action_pressed("Right"):
+		position.x += speed * delta
